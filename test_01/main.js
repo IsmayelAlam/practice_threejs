bc6635ca801f17cecd73bbd17f4428dec9a8b3de
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-16 21:33:51 +0600

    add transformation to mesh

--- test_01/main.js
+++ test_01/main.js
@@ -6,21 +6,62 @@ const canvas = document.querySelector(".webgl");
 
 const scene = new THREE.Scene();
 
-const geo = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geo, material);
+//add single mesh
+// const geo = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geo, material);
+// scene.add(mesh);
+// // position
+// // mesh.position.z = -3;
+// mesh.position.x = -1;
+// mesh.position.y = -1;
+// // scale
+// /* mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.05;*/
+// mesh.scale.set(2.5, 0.5, 0.5);
+// // rotation
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = 0.25 * Math.PI;
+// mesh.rotation.y = 0.25 * Math.PI;
 
-scene.add(mesh);
+//add group mesh
+const group = new THREE.Group();
+scene.add(group);
+
+const cube01 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const cube02 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+const cube03 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+cube01.position.set(-2, 0, 0);
+cube03.position.set(2, 0, 0);
+
+group.add(cube01, cube02, cube03);
+
+group.rotation.z = 0.1 * Math.PI;
+group.scale.y = 1.5;
+group.position.y = -0.5;
+
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 const size = {
-  width: 800,
-  height: 600,
+  width: innerWidth,
+  height: innerHeight,
 };
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
-camera.position.z = 3;
-// camera.position.x = 1;
-// camera.position.y = 1;
 scene.add(camera);
+camera.position.z = 3;
+// camera.lookAt(mesh.position);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(size.width, size.height);
