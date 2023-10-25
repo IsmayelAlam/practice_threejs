823a4ef9374f02fcaceaf6ad851c330be0066c27
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-25 15:05:45 +0600

    add base house meshs

--- test_03/main.js
+++ test_03/main.js
@@ -15,38 +15,57 @@ let aspectRatio = size.width / size.height;
 // Light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 const spotLight = new THREE.SpotLight(0xf0f2f0, 1);
+const ambientLight = new THREE.AmbientLight(0xf0f2f0, 2);
 
-scene.add(directionalLight, spotLight);
+scene.add(directionalLight, ambientLight);
 
 // material
 const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
 material.roughness = 0.5;
 
 //add group mesh
-const group = new THREE.Group();
+const cube = new THREE.BoxGeometry(1, 1, 1);
+const plane = new THREE.PlaneGeometry(1, 1, 1, 1);
+const cone = new THREE.ConeGeometry(1, 1, 4);
 
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 24), material);
+const ground = new THREE.Mesh(plane, material);
+ground.position.set(0, 0, 0);
+ground.scale.set(20, 20);
+ground.rotation.set(-Math.PI * 0.5, 0, 0);
 
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(7.5, 7.5, 100, 100),
-  material
-);
-plane.position.set(0, -1, 0);
-plane.rotation.set(-Math.PI * 0.5, 0, 0);
+const house = new THREE.Group();
 
-group.add(sphere, plane);
-scene.add(group);
+const walls = new THREE.Mesh(cube, material);
+walls.position.set(0, 1, 0);
+walls.scale.set(4, 2, 4);
 
+const roof = new THREE.Mesh(cone, material);
+roof.scale.set(3.5, 1, 3.5);
+roof.position.set(0, 2.5, 0);
+roof.rotation.set(0, -Math.PI * 0.25, 0);
+
+const door = new THREE.Mesh(plane, material);
+door.position.set(0, 0.5, 2.001);
+
+house.add(walls, roof, door);
+
+scene.add(ground, house);
+
+// Camera
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
-camera.position.z = 4;
+camera.position.set(-2, 1, 10);
+camera.lookAt(house.position);
 scene.add(camera);
 
+// controls
 const controls = new OrbitControls(camera, canvas);
 
+// renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 3));
 
+// animations
 function animate() {
   controls.update();
   renderer.render(scene, camera);
