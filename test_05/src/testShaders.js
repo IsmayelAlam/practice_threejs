d7183694d4a13e276dae75704ccb06ba23ccf8fe
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-30 12:17:03 +0600

    add base shader config and glsl file for raw shader material

--- test_05/src/testShaders.js
+++ test_05/src/testShaders.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "dat.gui";
+import testVertexShader from "./shaders/vertex.glsl";
+import testFragmentShader from "./shaders/fragment.glsl";
 
 const canvas = document.querySelector(".webgl");
 
@@ -12,15 +14,22 @@ const size = {
 };
 let aspectRatio = size.width / size.height;
 
+// shader
+// const material = new THREE.MeshBasicMaterial({ wireframe: true });
+const material = new THREE.RawShaderMaterial({
+  vertexShader: testVertexShader,
+  fragmentShader: testFragmentShader,
+});
+
 // add mesh
-const material = new THREE.MeshBasicMaterial();
-const geo = new THREE.PlaneGeometry(1, 1);
+const geo = new THREE.PlaneGeometry(1, 1, 32, 32);
 const mesh = new THREE.Mesh(geo, material);
 scene.add(mesh);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
-camera.position.set(0, 1, 2);
+camera.position.set(0, 0, 1.5);
+camera.lookAt(mesh.position);
 scene.add(camera);
 
 // controls
