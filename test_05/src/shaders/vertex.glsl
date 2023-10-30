f3f79163634a7f8c61a28b73e651fbde75c8627e
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-30 14:00:05 +0600

    create a simple wave mesh shader

--- test_05/src/shaders/vertex.glsl
+++ test_05/src/shaders/vertex.glsl
@@ -1,9 +1,23 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform vec2 uFreq;
+uniform float uTime;
 
 attribute vec3 position;
+attribute float aRandom;
+
+varying vec3 vColor;
 
 void main(){
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+  vec4 modelPos = modelMatrix * vec4(position, 1.0);
+  modelPos.z += sin(modelPos.x * uFreq.x + uTime) * 0.1;
+  modelPos.z += sin(modelPos.y * uFreq.y + uTime) * 0.1;
+
+  // vec4 viewPos = viewMatrix * modelPos;
+  // vec4 projectionPos = projectionMatrix * viewPos;
+
+  gl_Position = projectionMatrix * viewMatrix * modelPos;
+
+  vColor = position;
 }
--- test_05/src/testShaders.js
+++ test_05/src/testShaders.js
@@ -1,10 +1,18 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "dat.gui";
+import Stats from "three/addons/libs/stats.module.js";
+
 import testVertexShader from "./shaders/vertex.glsl";
 import testFragmentShader from "./shaders/fragment.glsl";
 
 const canvas = document.querySelector(".webgl");
+const gui = new dat.GUI();
+
+// performance monitor
+const stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb
+document.body.appendChild(stats.dom);
 
 const scene = new THREE.Scene();
 
@@ -15,14 +23,28 @@ const size = {
 let aspectRatio = size.width / size.height;
 
 // shader
-// const material = new THREE.MeshBasicMaterial({ wireframe: true });
 const material = new THREE.RawShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
+  uniforms: {
+    uFreq: { value: new THREE.Vector2(10, 5) },
+    uTime: { value: 0 },
+    uColor: { value: new THREE.Color("green") },
+  },
+  side: THREE.DoubleSide,
+  transparent: true,
+  // wireframe: true,
 });
 
+gui.add(material.uniforms.uFreq.value, "x", 5, 25, 0.5).name("FreqX");
+gui.add(material.uniforms.uFreq.value, "y", 0, 20, 0.5).name("FreqY");
+
 // add mesh
-const geo = new THREE.PlaneGeometry(1, 1, 32, 32);
+const geo = new THREE.PlaneGeometry(1.5, 1, 32, 32);
+const aRandom = geo.attributes.position.array.map(() => Math.random());
+const aColor = geo.attributes.position.array.map(() => Math.random());
+geo.setAttribute("aRandom", new THREE.BufferAttribute(aRandom, 1));
+geo.setAttribute("aColor", new THREE.BufferAttribute(aColor, 1));
 const mesh = new THREE.Mesh(geo, material);
 scene.add(mesh);
 
@@ -40,10 +62,19 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 3));
 
+// time
+const time = new THREE.Clock();
+
 // animations
 function animate() {
+  stats.begin();
+  const elapsedTime = time.getElapsedTime();
+
+  material.uniforms.uTime.value = elapsedTime;
+
   controls.update();
   renderer.render(scene, camera);
+  stats.end();
   window.requestAnimationFrame(animate);
 }
 animate();
@@ -56,9 +87,4 @@ addEventListener("resize", (e) => {
   camera.aspect = aspectRatio;
   camera.updateProjectionMatrix();
   renderer.setSize(size.width, size.height);
-});
-addEventListener("dblclick", (e) => {
-  document.fullscreenElement
-    ? document.exitFullscreen()
-    : canvas.requestFullscreen();
 });
