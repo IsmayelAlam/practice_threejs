cfa8317c0985686aafc581a0ff9de6c1eb88adf9
Ismayel Alam <ismayelalam0@gmail.com>
2023-11-07 22:35:19 +0600

    add keyboard control

--- test_08/src/Player.jsx
+++ test_08/src/Player.jsx
@@ -1,12 +1,54 @@
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
+import { useEffect, useRef } from "react";
 
 export default function Player() {
   const [subKeys, getKeys] = useKeyboardControls();
+  const body = useRef();
 
-  useFrame(() => {
-    const { forward, backward, leftward, rightward, jump } = getKeys();
+  useEffect(() => {
+    const unSubJump = subKeys(
+      (state) => state.jump,
+      (value) => {
+        const origin = body.current.translation();
+        if (value && origin.y < 0.5)
+          body.current.applyImpulse({ x: 0, y: 0.5, z: 0 });
+      }
+    );
+    return () => {
+      unSubJump();
+    };
+  }, []);
+
+  useFrame((state, delta) => {
+    const { forward, backward, leftward, rightward } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 0.5 * delta;
+    const torqueStrength = 0.2 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+      torque.x -= torqueStrength;
+    }
+    if (backward) {
+      impulse.z += impulseStrength;
+      torque.x += torqueStrength;
+    }
+    if (leftward) {
+      impulse.x -= impulseStrength;
+      torque.z += torqueStrength;
+    }
+    if (rightward) {
+      impulse.x += impulseStrength;
+      torque.z -= torqueStrength;
+    }
+
+    body.current.applyImpulse(impulse);
+    body.current.applyTorqueImpulse(torque);
   });
 
   return (
@@ -15,6 +57,10 @@ export default function Player() {
       position={[0, 1, 0]}
       restitution={0.2}
       friction={1}
+      linearDamping={0.5}
+      angularDamping={0.5}
+      ref={body}
+      canSleep={false}
     >
       <mesh castShadow>
         <icosahedronGeometry args={[0.3, 1]} />
--- test_08/src/main.jsx
+++ test_08/src/main.jsx
@@ -3,19 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
+import { KeyboardControls } from "@react-three/drei";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Canvas
-      shadows
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [2.5, 4, 6],
-      }}
+    <KeyboardControls
+      map={[
+        { name: "forward", keys: ["ArrowUp", "KeyW"] },
+        { name: "backward", keys: ["ArrowDown", "KeyS"] },
+        { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+        { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+        { name: "jump", keys: ["Space"] },
+      ]}
     >
-      <App />
-    </Canvas>
+      <Canvas
+        shadows
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [2.5, 4, 6],
+        }}
+      >
+        <App />
+      </Canvas>
+    </KeyboardControls>
   </React.StrictMode>
 );
